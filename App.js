49d58319fc5d8ab78f4c5c73765975e100bc2ad5
jack.ding <jackding96@outlook.com>
2018-12-17 00:01:49 -0500

    inital work on dummy layout

--- App.js
+++ App.js
@@ -5,7 +5,11 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Boobs</Text>
+        <View>
+          <Text style={styles.mainText}>Today</Text>
+          <Text style={styles.secondaryText}>$26.32</Text>  
+          {/* <Text style={styles.fun}>Boobs</Text>         */}
+        </View>
       </View>
     );
   }
@@ -14,8 +18,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40,
+    marginLeft: 20
   },
+  mainText: {
+    fontSize: 34,
+    marginBottom: 5,
+    fontFamily: 'System',
+    fontWeight: "bold",
+  },
+  secondaryText: {
+    fontSize: 20,
+    color: '#4CD964',
+    fontWeight: "700"
+  },
+  fun: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "pink"
+  }
 });
