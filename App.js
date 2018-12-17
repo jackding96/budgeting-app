fe180bdff679e3f0b135dded0ba0cd45f508b925
jack.ding <jackding96@outlook.com>
2018-12-17 12:24:45 -0500

    move out styles to separate file

--- App.js
+++ App.js
@@ -1,40 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
+import styles from './App.styles.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.mainText}>Today</Text>
-          <Text style={styles.secondaryText}>$26.32</Text>  
-          {/* <Text style={styles.fun}>Boobs</Text>         */}
+      <ScrollView showsVerticalScrollIndicator='False'>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <Text style={styles.mainText}>Today</Text>
+            <Text style={styles.secondaryText}>$26.32</Text>  
+          </View>
+          <View style={styles.list}>
+            <View style={styles.listItem}>
+              <Text>7/11</Text>
+              <Text>Food</Text>
+              <Text>$4.56</Text>
+            </View>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 40,
-    marginLeft: 20
-  },
-  mainText: {
-    fontSize: 34,
-    marginBottom: 5,
-    fontFamily: 'System',
-    fontWeight: "bold",
-  },
-  secondaryText: {
-    fontSize: 20,
-    color: '#4CD964',
-    fontWeight: "700"
-  },
-  fun: {
-    fontSize: 100,
-    fontWeight: "bold",
-    color: "pink"
-  }
-});
---
+++ App.styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20,
+  },
+  mainText: {
+    fontSize: 34,
+    marginBottom: 4,
+    fontFamily: 'System',
+    fontWeight: "bold",
+  },
+  secondaryText: {
+    fontSize: 20,
+    opacity: 0.6,
+    fontWeight: "600"
+  },
+  header: {
+    paddingLeft: 20,
+    marginTop: 20,
+    backgroundColor: 'pink',
+  },
+  list: {
+
+  },
+  listItem: {
+    backgroundColor: 'blue'
+  }
+});
